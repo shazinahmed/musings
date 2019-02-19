c12336711b273e4020d1ab87b1ce42fef259a3ca
Shazin Shafi Ahamed <shazinahmed@gmail.com>
2019-02-19 18:07:09 -0500

    First commit

---
+++ Musings/src/shazinltc/warmup/jumpingclouds/Solution.java
@@ -0,0 +1,56 @@
+package shazinltc.warmup.jumpingclouds;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+    	int count = 0;
+    	for (int i=0; i<c.length - 1; i++)
+    	{
+    		if (i<c.length-2 && c[i+2] != 1)
+    		{
+    			i = i+1;
+    		}
+    		count = count + 1;
+    	}
+    	return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+        int n = 6;
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[] {0,0,0,0,1,0};
+
+//        String[] cItems = scanner.nextLine().split(" ");
+//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+//
+//        for (int i = 0; i < n; i++) {
+//            int cItem = Integer.parseInt(cItems[i]);
+//            c[i] = cItem;
+//        }
+
+        int result = jumpingOnClouds(c);
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+//
+//        scanner.close();
+    }
+}
+
