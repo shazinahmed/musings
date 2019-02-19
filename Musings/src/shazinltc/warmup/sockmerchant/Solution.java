9bfc49eafe9d33c703cf50d8ec3dede9d07f4134
Shazin Shafi Ahamed <shazinahmed@gmail.com>
2019-02-19 17:31:13 -0500

    First version

---
+++ Musings/src/shazinltc/warmup/sockmerchant/Solution.java
@@ -0,0 +1,56 @@
+package shazinltc.warmup.sockmerchant;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        Map<Integer, Integer> colorMap = new HashMap<>();
+        Integer color;
+        for (int i=0; i<ar.length; i++)
+        {
+        	//ad
+        	color = ar[i];
+        	Integer count = colorMap.get(color) != null ? colorMap.get(color) : 0;
+        	colorMap.put(color, count + 1);
+        }
+        return colorMap.values().stream().mapToInt(i -> i/2).sum();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = 9;
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20};
+
+        //String[] arItems = scanner.nextLine().split(" ");
+        //scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+//        for (int i = 0; i < n; i++) {
+//            int arItem = Integer.parseInt(arItems[i]);
+//            ar[i] = arItem;
+//        }
+
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedWriter.close();
+//
+//        scanner.close();
+    }
+}
+
