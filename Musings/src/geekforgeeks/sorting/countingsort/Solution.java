5261e67bb649b29d35561a5be1c217e7b54c0767
Shazin Shafi Ahamed <shazinahmed@gmail.com>
2019-02-20 20:19:09 -0500

    Initial commit

--- Musings/src/geekforgeeks/sorting/countingsort/Solution.java
+++ Musings/src/geekforgeeks/sorting/countingsort/Solution.java
@@ -6,8 +6,12 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[] {1, 4, 1, 2, 7, 5, 2};
+		int[] arr = new int[] {-5, -10, 0, -3, 8, 5, -1, 10};
 		countingSort(arr);
+		for (int i=0; i<arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
 	}
 	
 	private static void countingSort(int[] arr)
